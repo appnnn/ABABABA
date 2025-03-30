51d5eef1978327f8d71d9fcfaeee36e985bffd9b
Mahagamage H.D. <harindulneth@gmail.com>
2025-03-31 00:53:58 +0530

    bvrdfd

---
+++ login/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+    {
+        
+        if (string.IsNullOrEmpty(UsernameTextBox.Text))
+        {
+            contentPanel.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            contentPanel.Visibility = Visibility.Collapsed;
+        }
+    }
+    private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        contentPanelpass.Visibility = string.IsNullOrEmpty(PasswordBox.Password) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            DragMove(); 
+        }
+    }
+
+    private void MinimizeWindow(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
+
+    private void CloseWindow(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+    private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+
+
+}
